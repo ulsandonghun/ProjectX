50bd841cea02328d321c21e5ae344b74449f0069
ulsandonghun <donghoon141@naver.com>
2023-10-30 18:09:43 +0900

    [문제 3 ] : 오류해결 입력 값 미스매치

--- src/main/java/org/example/week1/HelloBiodome03.java
+++ src/main/java/org/example/week1/HelloBiodome03.java
@@ -1,7 +1,7 @@
 package org.example.week1;
 
 public class HelloBiodome03 {
-    Double pie=3.14;
+    static Double pie=3.14;
 
     public static void main(String[] args) {
 
@@ -18,17 +18,35 @@ public class HelloBiodome03 {
             System.out.println("d[i] = " + d[i]);
         }
 
+        double result;
+        double cal=calculate(sqrt(d[1]),d[0]);
+        result=health(0.415,cal,d[2]/(pie*pie));
+
+        System.out.println(result);
+
 
 
     }
 
     public static double sqrt(double num) {
+        System.out.println("루트 넘버 = " + Math.sqrt(num));
         return Math.sqrt(num);
     }
 
     public static double calculate(double a, double b) {
 
+        System.out.println("습도 - 온도 절댓값 = " + Math.abs(a-b));
+        return Math.abs(a - b);
+    }
 
+    public static double health(double a,double b,double c) {
 
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+
+        System.out.println("a * b = " + a * b);
+        System.out.println("(a * b) + c = " + (a * b) + c);
+        return (a * b) + c;
     }
 }
