79b76faf9859e241b71db1590c8ffe179f17aaa3
ulsandonghun <donghoon141@naver.com>
2023-11-02 11:06:07 +0900

    [문제 2] : 보너스 문제 해결

--- src/main/java/org/example/week1/HelloBiodome02.java
+++ src/main/java/org/example/week1/HelloBiodome02.java
@@ -11,12 +11,17 @@ public class HelloBiodome02 {
 
         String str = String.join(" ", args);
         String[] s = str.split(" ");
+        int[] arr = new int[3];
         int result=0;
         for (int i = 0; i < s.length; i++) {
-           result+= Integer.valueOf(s[i]);
-            System.out.println("s[i] = " + s[i]);
+            Integer result1 = Integer.valueOf(s[i]);
+            arr[i]= result1;
+            result+= result1;
         }
+
+
         System.out.println("총 에너지 사용량은 "+result+"입니다.");
+        System.out.println("태양광 " + ((double)arr[0]/result)*100 + "%, 풍력 " + (double)arr[1]/result*100 + "%, 지열 " + (double)arr[2]/result*100 + "%");
 
 
     }
