a64becd0db985d4a30d518d89d8e538bc139d05f
ulsandonghun <donghoon141@naver.com>
2023-11-06 17:39:33 +0900

    [feat] : 배열을 순회하면서 Set 사용하지 않고 중복 제거 기능

---
+++ src/main/java/org/example/week2/RoadToBiodome01.java
@@ -0,0 +1,37 @@
+package org.example.week2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RoadToBiodome01 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        String[] stringArray1 = args[0].replaceAll("[^0-9]+", " ").trim().split("\\s+");
+        int[] array1 = new int[stringArray1.length];
+        for (int i = 0; i < stringArray1.length; i++) {
+            array1[i] = Integer.parseInt(stringArray1[i]);
+        }
+        for (int i : array1) {
+            int count = countNum(array1, i);
+            if (count == 1) {
+                System.out.println(i);
+            }
+        }
+        System.out.println(list.get(0));
+    }
+
+    static int countNum(int[] list,int findCountNum) {
+        int size= list.length;
+        int count=0;
+        for (int i : list) {
+            if(i==findCountNum){
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
