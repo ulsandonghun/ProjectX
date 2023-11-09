44c13248d6ab3c8d8f8e4f4de86503d5491cf689
ulsandonghun <donghoon141@naver.com>
2023-11-09 11:50:48 +0900

    [fix] : 배열의 값 30 이상일때만 계산되도록 수정

--- src/main/java/org/example/week2/RoadToBiodome06.java
+++ src/main/java/org/example/week2/RoadToBiodome06.java
@@ -1,39 +1,33 @@
 package org.example.week2;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class RoadToBiodome06 {
     public static void main(String[] args) {
 
         String input=String.join(" ",args);
-        // 문자열에서 배열을 분리
         String[] arrayStrings = input.split("\\]\\s+\\[");
 
-        // 첫 번째 배열 생성
-        String[] stringArray1 = arrayStrings[0].replaceAll("[^0-9]+", " ").trim().split("\\s+");
-        int[] array1 = new int[stringArray1.length];
-        for (int i = 0; i < stringArray1.length; i++) {
-            array1[i] = Integer.parseInt(stringArray1[i]);
-        }
-        Arrays.sort(array1);
+        int[] array1 = filterAndConvertArray(arrayStrings[0]);
+        int[] array2 = filterAndConvertArray(arrayStrings[1]);
 
-        // 두 번째 배열 생성
-        String[] stringArray2 = arrayStrings[1].replaceAll("[^0-9]+", " ").trim().split("\\s+");
-        int[] array2 = new int[stringArray2.length];
-        for (int i = 0; i < stringArray2.length; i++) {
-            array2[i] = Integer.parseInt(stringArray2[i]);
-        }
-        Arrays.sort(array2);
-
-        // 평균 계산
         double mean = calculateMean(array1, array2);
-
-        // 중앙값 계산
         double median = calculateMedian(array1, array2);
 
         System.out.println("Mean : " + mean);
         System.out.println("Median : " + median);
     }
+
+    private static int[] filterAndConvertArray(String arrayString) {
+        String[] stringArray = arrayString.replaceAll("[^0-9]+", " ").trim().split("\\s+");
+        return IntStream.range(0, stringArray.length)
+                .map(i -> Integer.parseInt(stringArray[i]))
+                .filter(num -> num >= 30)
+                .sorted()
+                .toArray();
+    }
+
     public static double calculateMean(int[] array1, int[] array2) {
         double sum = 0;
         for (int num : array1) {
@@ -44,34 +38,31 @@ public class RoadToBiodome06 {
         }
         return sum / (array1.length + array2.length);
     }
+
     public static double calculateMedian(int[] array1, int[] array2) {
         int totalLength = array1.length + array2.length;
         if (totalLength % 2 == 1) {
-            //홀수일 경우 중앙값은 두 값크기의 절반 +1이다.
             return findKth(array1, array2, totalLength / 2 + 1);
         } else {
             return (findKth(array1, array2, totalLength / 2) + findKth(array1, array2, totalLength / 2 + 1)) / 2.0;
         }
     }
+
     public static int findKth(int[] array1, int[] array2, int k) {
         int length1 = array1.length, length2 = array2.length;
         int index1 = 0, index2 = 0;
 
         while (true) {
-            // array1이 빈 배열인 경우
             if (index1 == length1) {
                 return array2[index2 + k - 1];
             }
-            // array2가 빈 배열인 경우
             if (index2 == length2) {
                 return array1[index1 + k - 1];
             }
-            // k가 1인 경우
             if (k == 1) {
                 return Math.min(array1[index1], array2[index2]);
             }
 
-            // array1과 array2에서 새로운 인덱스 계산
             int newIndex1 = Math.min(index1 + k / 2 - 1, length1 - 1);
             int newIndex2 = Math.min(index2 + k / 2 - 1, length2 - 1);
 
@@ -84,5 +75,4 @@ public class RoadToBiodome06 {
             }
         }
     }
-
 }
