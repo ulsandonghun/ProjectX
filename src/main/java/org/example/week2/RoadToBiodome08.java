641c65b23b49da022cfaa8968600764b00f2d912
ulsandonghun <donghoon141@naver.com>
2023-11-09 11:41:26 +0900

    큐 클래스 삭제 및 내부화

--- src/main/java/org/example/week2/RoadToBiodome08.java
+++ src/main/java/org/example/week2/RoadToBiodome08.java
@@ -1,35 +1,31 @@
 package org.example.week2;
 
 public class RoadToBiodome08 {
+    private static int maxSize;
+    private static int front;
+    private static int rear;
+    private static int[] queueArray;
+
     public static void main(String[] args) {
-        DynamicQueue dynamicQueue = new DynamicQueue(10);
+        init(10);
         for (String str : args) {
-            dynamicQueue.enqueue(Integer.valueOf(str));
+            enqueue(Integer.valueOf(str));
         }
-        while (!dynamicQueue.isEmpty()) {
-            System.out.println("자원 " + dynamicQueue.dequeue() + "을 제공했습니다.");
+        while (!isEmpty()) {
+            System.out.println("자원 " + dequeue() + "을 제공했습니다.");
         }
         System.out.println("모든 요청이 처리되었습니다.");
     }
 
-
-}
-class DynamicQueue {
-    private int maxSize;
-    private int front;
-    private int rear;
-    private int[] queueArray;
-
-    public DynamicQueue(int size) {
-        this.maxSize = size;
-        this.front = 0;
-        this.rear = -1;
-        this.queueArray = new int[maxSize];
+    public static void init(int size) {
+        maxSize = size;
+        front = 0;
+        rear = -1;
+        queueArray = new int[maxSize];
     }
 
-    public void enqueue(int value) {
+    public static void enqueue(int value) {
         if (rear == maxSize - 1) {
-            // 큐가 가득 찼으므로 큐의 크기를 늘립니다.
             maxSize += 10;
             int[] newQueueArray = new int[maxSize];
             System.arraycopy(queueArray, 0, newQueueArray, 0, queueArray.length);
@@ -39,8 +35,7 @@ class DynamicQueue {
         queueArray[++rear] = value;
     }
 
-    // dequeue, peek, isEmpty 메서드는 앞서 구현한 것과 동일합니다.
-    public int dequeue() {
+    public static int dequeue() {
         if (front > rear) {
             System.out.println("큐가 비었습니다.");
             return -1;
@@ -49,12 +44,7 @@ class DynamicQueue {
         }
     }
 
-    public int peek() {
-        return queueArray[front];
-    }
-
-    public boolean isEmpty() {
+    public static boolean isEmpty() {
         return (front > rear);
     }
-
 }
